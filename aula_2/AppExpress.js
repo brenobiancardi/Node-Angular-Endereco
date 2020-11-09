96bf3c585fc5b38ac01ef3224d5718e93b0e6f68
brenobiancardi <brenobiancardi@outlook.com>
2020-11-08 22:10:47 -0300

    Reorganização do projeto

--- aula_2/AppExpress.js
+++ aula_2/AppExpress.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-
-app.engine('handlebars', handlebars({defineLayout: 'main'}) );
-app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended: false}));
+app.engine("handlebars", handlebars({ defineLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 
 const swaggerOptions = {
   swaggerDefinition: {
@@ -20,27 +20,21 @@ const swaggerOptions = {
       title: "API Programação para Internet",
       description: "API de manipulação de informações da aplicação",
       contact: {
-        name: "Bruno Stoll" // nome e sobrenome do aluno
+        name: "Bruno Stoll", // nome e sobrenome do aluno
       },
-      servers: [ "http://localhost:8777" ]
-    }
+      servers: ["http://localhost:8777"],
+    },
   },
-  apis: [ "RotaUsuario.js", "RotaTarefa.js" ]
+  apis: ["RotaUsuario.js", "RotaTarefa.js"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // APIs
-var rtUsuario   = require('./RotaUsuario')(app);
-var rtTarefa    = require('./RotaTarefa')(app);
+var rtUsuario = require("./RotaUsuario")(app);
+var rtTarefa = require("./RotaTarefa")(app);
 
-
-
-app.listen(8777, function() {
-	console.log("Servidor executando")
+app.listen(8777, function () {
+  console.log("Servidor executando");
 });
-
-
-
-
